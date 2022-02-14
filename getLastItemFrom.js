1895d51173724cb18712b47e69a7154c413f7f12
gcarpio1718 <gcarpio1221@gmail.com>
2022-02-14 12:16:41 -0500

    change Last item

---
+++ getLastItemFrom.js
@@ -0,0 +1,13 @@
+let p1 = "Sultan";
+let p2 = "Queen";
+let p3 = "Prince";
+
+let names = [p1, p2, p3];
+
+console.log(names);
+
+let namesLength = names.length;
+
+let lastItem = names[namesLength-1];
+
+console.log("The last name in the array is: " + lastItem);
