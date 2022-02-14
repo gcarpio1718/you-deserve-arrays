9fc9ee3e3889ee14b35f74391728aa15ef31602e
gcarpio1718 <gcarpio1221@gmail.com>
2022-02-14 15:25:15 -0500

    change Long

---
+++ isLongList.js
@@ -0,0 +1,14 @@
+
+let userArray = JSON.parse(prompt("Enter an array: "));
+
+let atLeast10 = false;
+
+let uAlength = userArray.length;
+
+if (uAlength >= 10) {
+
+    atLeast10 = true
+    console.log(atLeast10)
+}else{
+    console.log(atLeast10)
+}
