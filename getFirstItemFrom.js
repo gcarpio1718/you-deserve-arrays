a7789e98750885784f2d245c1fe2c551476d721e
gcarpio1718 <gcarpio1221@gmail.com>
2022-02-14 12:15:58 -0500

    Change First

---
+++ getFirstItemFrom.js
@@ -0,0 +1,10 @@
+let p1 = "Sultan";
+let p2 = "Queen";
+let p3 = "Prince";
+
+let names = [p1, p2, p3];
+
+console.log(names);
+let firstItem = names[0];
+
+console.log("The first name in the array is: " + firstItem);
